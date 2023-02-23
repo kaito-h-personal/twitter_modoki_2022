b07e30f064a2a33054c35dfb96f6289a6763ddc8
kaito-h <kaito.hayafune@pasonadigisol.jp>
2023-02-23 13:12:41 +0900

    画面イメージ作成

--- src/front/frontend/src/App.tsx
+++ src/front/frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,27 +15,9 @@ function App() {
 
   return (
     <div className="App">
-      <div>{posts.hello}</div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR!!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>👤&lt;{posts.hello}</div>
+      <div>👤&lt;起きた〜</div>
+      <div>👤&lt;お腹すいた〜</div>
     </div>
   );
 }
