b3ab3206351356250dea0e33ec016ce8bd1276bb
kaito-h <kaito.hayafune@pasonadigisol.jp>
2023-02-23 22:08:04 +0900

    mui導入サンプル

--- src/front/frontend/src/App.tsx
+++ src/front/frontend/src/App.tsx
@@ -1,5 +1,15 @@
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import "./App.css";
+
 // import reactLogo from "./assets/react.svg";
 
 function App() {
@@ -16,6 +26,67 @@ function App() {
 
   return (
     <div className="App">
+      <Container maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h4">
+            ログイン
+          </Typography>
+
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="メールアドレス"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="パスワード"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              ログイン
+            </Button>
+
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  パスワードを忘れた
+                </Link>
+              </Grid>
+
+              <Grid item>
+                <Link href="#" variant="body2">
+                  新規登録
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
       <div>👤&lt;{posts.hello}</div>
       <div>👤&lt;起きた〜</div>
       <div>👤&lt;お腹すいた〜</div>
