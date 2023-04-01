d0972ab41136f8d563a80bea127077d1bc7e18bc
kaito-h-personal <unrequal@gmail.com>
2023-04-02 03:14:55 +0900

    構造化まで

--- src/api/main.go
+++ src/api/main.go
@@ -5,7 +5,7 @@ import (
     "fmt"
     "net/http"
     "io/ioutil"
-    // "encoding/json"
+    "encoding/json"
 )
 
 type Tweet struct {
@@ -15,25 +15,43 @@ type Tweet struct {
     Text       string `json:"text"`
 }
 
-type Result struct {
-    Tweets []Tweet `json:"result"`
-}
+
 
 type Response struct {
     Time   string `json:"time"`
     Status string `json:"status"`
-    Result Result `json:"result"`
+    Result []Tweet `json:"result"`
 }
 
 func main() {
-    body, err := fetch_tweets()
+    jsonString, err := fetch_tweets()
     if err != nil {
         fmt.Println(err)
         return
     }
 
-    fmt.Println("!")
-    fmt.Println(body)
+    // fmt.Println("!")
+    fmt.Println(jsonString)
+
+    var responses []Response
+    err = json.Unmarshal([]byte(jsonString), &responses)
+    if err != nil {
+        fmt.Println("@@")
+        fmt.Println(err)
+        return
+    }
+
+    for _, response := range responses {
+        fmt.Println("Time:", response.Time)
+        fmt.Println("Status:", response.Status)
+        for _, tweet := range response.Result {
+            fmt.Println("Auther:", tweet.Auther)
+            fmt.Println("Created At:", tweet.CreatedAt)
+            fmt.Println("ID:", tweet.ID)
+            fmt.Println("Text:", tweet.Text)
+        }
+    }
+    fmt.Println("Fin.")
 }
 
 func fetch_tweets() (string, error) {
@@ -74,9 +92,9 @@ func sendQuery(query string) (string, error) {
     }
     defer resp.Body.Close()
 
-    fmt.Println(resp.Status)
-    fmt.Println("~")
-    fmt.Println(resp)
+    // fmt.Println(resp.Status)
+    // fmt.Println("~")
+    // fmt.Println(resp)
 
     body, err := ioutil.ReadAll(resp.Body)
     if err != nil {
