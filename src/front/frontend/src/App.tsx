85dfbc7427ab2cf67c9210ee990c5eaf54fe21f9
kaito-h-personal <unrequal@gmail.com>
2023-04-02 23:14:11 +0900

    tweetsを表示

--- src/front/frontend/src/App.tsx
+++ src/front/frontend/src/App.tsx
@@ -34,89 +34,46 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1>Tweets</h1>
-        <ul>
-          {tweets.map((tweet) => (
-            <li key={tweet.id}>
-              <p>{tweet.text}</p>
-              <p>Auther: {tweet.auther}</p>
-              <p>Created At: {tweet.created_at}</p>
-            </li>
-          ))}
-        </ul>
+        {tweets.map((tweet) => (
+          <div key={tweet.id}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardHeader
+                avatar={
+                  <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                    Ri
+                  </Avatar>
+                }
+                action={
+                  <IconButton aria-label="settings">
+                    <MoreVertIcon />
+                  </IconButton>
+                }
+                title={tweet.auther}
+                subheader={tweet.created_at}
+              />
+              {/* <CardMedia
+          component="img"
+          height="194"
+          image="/static/images/cards/paella.jpg"
+          alt="Paella dish"
+        /> */}
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  {tweet.text}
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                  <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                  <ShareIcon />
+                </IconButton>
+              </CardActions>
+            </Card>
+          </div>
+        ))}
       </div>
-
-      <Card sx={{ maxWidth: 345 }}>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              Ri
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="梨香子"
-          subheader="2023/02/04"
-        />
-        {/* <CardMedia
-          component="img"
-          height="194"
-          image="/static/images/cards/paella.jpg"
-          alt="Paella dish"
-        /> */}
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            起きた〜
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="share">
-            <ShareIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
-
-      <Card sx={{ maxWidth: 345 }}>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              K
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="Kouji"
-          subheader="2023/02/02"
-        />
-        {/* <CardMedia
-          component="img"
-          height="194"
-          image="/static/images/cards/paella.jpg"
-          alt="Paella dish"
-        /> */}
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            お腹すいた〜
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="share">
-            <ShareIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
     </div>
   );
 }
