a59c0181ac62f4d8ba08ef6014c3e16ac41e8794
kaito-h-personal <unrequal@gmail.com>
2023-05-04 01:19:43 +0900

    ログインフォームを拝借

--- src/front/frontend/src/Login.tsx
+++ src/front/frontend/src/Login.tsx
@@ -1,15 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
 
-const MyComponent: React.FC = () => {
+const App = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to="/App">
-        <p>To App Page</p>
-      </Link>
-    </div>
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h4">
+          ログイン
+        </Typography>
+
+        <Box component="form" noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="メールアドレス"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="パスワード"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            ログイン
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
-export default MyComponent;
+export default App;
